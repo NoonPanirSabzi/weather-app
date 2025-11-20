0f0f487dae23cc1eb2dd2564544dacf4aaf3c351
Amin Forouzan <amin.pqr@gmail.com>
2025-11-20 17:44:40 +0330

    feat(ui): create App layout with current components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
-import './App.css'
+import "./App.css";
+import { Header } from "./assets/components/Header";
+import { Search } from "./assets/components/Search";
+import { WeatherInfo } from "./assets/components/WeatherInfo";
 
 function App() {
-
   return (
-    <div>hello world</div>
-  )
+    <div className="flex flex-col gap-600 p-200">
+      <Header />
+      <h1 className="text-preset-2 mx-auto max-w-[20.625rem] text-center text-neutral-0">
+        How’s the sky looking today?
+      </h1>
+      <div className="flex flex-col gap-y-400">
+        <Search />
+        <WeatherInfo
+          location="Berlin, Germany"
+          date="Tuesday, Aug 5, 2025"
+          temperature="20"
+          weatherIcon="sunny"
+          feelsLike="18"
+          humidity="46"
+          wind="14"
+          precipitation="0"
+        />
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
